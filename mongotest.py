5a36aa06661707784122828b41f5e53d1625f85f
Admin <mrunal2210@yahoo.in>
2022-07-17 19:30:44 +0530

    Hello

---
+++ mongotest.py
@@ -0,0 +1,10 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://mrunalsawant:mongodb123.v2anxnk.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+
+kfhiruhkvn
+sdflkthlk
+msdnkddfbjk
